10ed4cbd4f772ec4a96f5ee9c91cc1d597a29108
Karen <karen.martirosyan2000@bk.ru>
2024-03-03 15:24:02 +0400

    full

--- src/App.js
+++ src/App.js
@@ -1,23 +1,159 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [allTodo, setAllTodo] = useState([]);
+  const [completed, setCompleted] = useState([]);
+  const [unCompleted, setUnCompleted] = useState([]);
+  const [currentValue, setCurrentValue] = useState("all");
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (currentValue === "completed") {
+      console.log("completed");
+      const completedState = [...allTodo];
+      const completedState1 = completedState.filter((el) => {
+        return el.isChecked === true;
+      });
+      setCompleted(completedState1);
+    } else if (currentValue === "unCompleted") {
+      console.log("unCompleted");
+      const unCompletedState = [...allTodo];
+      const unCompletedState1 = unCompletedState.filter((el) => {
+        return el.isChecked === false;
+      });
+      setUnCompleted(unCompletedState1);
+    }
+  }, [allTodo, currentValue]);
+
+  const todo = () => {
+    console.log(inputRef.current.value);
+    const obj = {
+      text: inputRef.current.value,
+      id: Math.random(),
+      isChecked: false,
+    };
+    setAllTodo([...allTodo, obj]);
+    inputRef.current.value = "";
+  };
+
+  const onChange = (elId) => {
+    const test = allTodo.map((el) => {
+      if (elId.id === el.id) {
+        el.isChecked = !el.isChecked;
+        return el;
+      }
+      return el;
+    });
+    setAllTodo(test);
+  };
+
+  const isDel = (elem) => {
+    const del = allTodo.filter((el) => {
+      return elem.id !== el.id;
+    });
+    setAllTodo(del);
+  };
+
+  const completedTodo = () => {
+    setCurrentValue("completed");
+  };
+
+  const unCompletedToodo = () => {
+    setCurrentValue("unCompleted");
+  };
+
+  const clearCheckedItem = () => {
+    const todoClearr = [...allTodo];
+    const clearTodo = todoClearr.filter((el) => {
+      return el.isChecked === false;
+    });
+    setAllTodo(clearTodo);
+  };
+
+  const currentArr = currentValue === "all" ? allTodo : currentValue === "completed" ? completed : unCompleted;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h2>My To Do List</h2>
+      <input placeholder="Title..." ref={inputRef}></input>
+      <button onClick={() => todo()}>Add</button>
+      {currentArr.map((element) => {
+        return (
+          <div
+            key={element.id}
+            style={{ display: "flex", justifyContent: "center" }}
+          >
+            <div
+              style={{
+                display: "flex",
+                backgroundColor: "red",
+                width: "273px",
+                padding: "8px",
+                marginBottom: "6px",
+              }}
+            >
+              <input
+                type={"checkbox"}
+                checked={element.isChecked}
+                onChange={() => onChange(element)}
+              />
+              <div>{element.text}</div>
+              <button onClick={() => isDel(element)}>x</button>
+            </div>
+          </div>
+        );
+      })}
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: " 10px",
+          marginBottom: "12px",
+          marginTop: "12px",
+        }}
+      >
+        <button
+          style={{
+            backgroundColor: currentValue === "all" ? "red" : "white",
+          }}
+          onClick={() => {
+            setCurrentValue("all");
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          All
+        </button>
+        <button
+          style={{
+            backgroundColor: currentValue === "completed" ? "red" : "white",
+          }}
+          onClick={() => {
+            completedTodo();
+          }}
+        >
+          Completed
+        </button>
+
+        <button
+          style={{
+            backgroundColor: currentValue === "unCompleted" ? "red" : "white",
+          }}
+          onClick={() => {
+            unCompletedToodo();
+          }}
+        >
+          UnCompleted
+        </button>
+
+        <button
+          onClick={() => {
+            clearCheckedItem();
+          }}
+        >
+          Clear Completed
+        </button>
+      </div>
     </div>
   );
 }
